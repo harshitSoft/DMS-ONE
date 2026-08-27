bbe9fb25ada86cd5a72e32f6847b6b503bbea71c
harshitSoft <harshit.nigam@itsoftlab.com>
2026-08-27 12:55:23 +0530

    cors changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,9 @@ const allowedOrigins = [
   "http://localhost:5175",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
-  "http://127.0.0.1:5175"
+  "http://127.0.0.1:5175",
+  "https://dms-one.netlify.app/",
+  "https://dms-one.netlify.app"
 ];
 
 const corsOptions = {
